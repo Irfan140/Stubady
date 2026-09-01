cf2ff41f12eb15c764aa2d64c9a1f9df574fd14d
Irfan Mehmud <irfanmehmud140@gmail.com>
2026-09-01 18:19:27 +0530

    chore: update projectId in app.config.ts for EAS configuration

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -103,7 +103,7 @@ const config: ExpoConfig = {
     router: {},
 
     eas: {
-      projectId: "3c58fbcc-ede2-4ea9-b211-fbba65c39cb9",
+     "projectId": "337168a4-5446-4f92-a8c7-e364f1e732a9"
     },
 
     EXPO_PUBLIC_CLERK_GOOGLE_WEB_CLIENT_ID:
